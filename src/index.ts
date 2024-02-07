9615ac1ac325980002c0cb62d26f044b06bbc00d
IrinaEnotova <irinkayashina@gmail.com>
2024-02-07 14:44:10 +0400

    feat: create simple server and json with data for test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import http from "http";
+import { default as data } from "./data/users.json" assert { type: "json" };
 
 const server = http.createServer((req, res) => {
-  console.log("111");
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(data));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "route was not found" }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
